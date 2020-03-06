5de2072b4a2f6f859a82a4281e6caedd407a1e5d
kullex <kullex@tutanota.com>
2020-03-06 17:52:08 +0800

    使用cat创建配置文件

--- Dockerfile
+++ Dockerfile
@@ -8,17 +8,11 @@ ENV V2RAY_PATH=ray
 RUN apk add --no-cache --virtual .build-deps ca-certificates curl \
  && curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip \
  && mkdir /usr/bin/v2ray /etc/v2ray \
+ && touch /etc/v2ray/config.json \
  && unzip /v2ray.zip -d /usr/bin/v2ray \
  && rm -rf /v2ray.zip /usr/bin/v2ray/*.sig /usr/bin/v2ray/doc /usr/bin/v2ray/*.json /usr/bin/v2ray/*.dat /usr/bin/v2ray/sys* \
  && chgrp -R 0 /etc/v2ray \
  && chmod -R g+rwX /etc/v2ray
-
-COPY config.json /etc/v2ray/config.json
-RUN sed -i -e "s#frontend_uuid#$FRONTEND_UUID#g" \
-    -e "s#backend_uuid#$BACKEND_UUID#g" \
-    -e "s#v2ray_port#$V2RAY_PORT#g" \
-    -e "s#v2ray_path#$V2RAY_PATH#g" \
-    /etc/v2ray/config.json
 
 ADD configure.sh /configure.sh
 RUN chmod +x /configure.sh
--- configure.sh
+++ configure.sh
@@ -1,11 +1,49 @@
 #!/bin/sh
 
-# Show V2Ray Config
-echo "======DEBUG======"
-echo $V2RAY_PORT
-echo $V2RAY_PATH
-echo $FRONTEND_UUID
-echo $BACKEND_UUID
+# New Config
+cat > /etc/v2ray/config.json << EOF
+{
+    "inbounds": [
+        {
+            "port": $V2RAY_PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$BACKEND_UUID",
+                        "alterId": 64
+                    }
+                ]
+            }
+        },
+        {
+            "port": 8188,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$FRONTEND_UUID",
+                        "alterId": 64
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "/$V2RAY_PATH"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        }
+    ]
+}
+EOF
+
 cat /etc/v2ray/config.json
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
